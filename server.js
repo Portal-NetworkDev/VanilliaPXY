751c523083c353719073c7fbbf3b2ff7464adf5a
Emma2114 <easypickleslol@gmail.com>
2026-09-07 03:28:00 -0400

    Shouldve fixed serverjs. why am i doing this at 3am.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import { server } from "./src/index.js";
 
 if (!process.env.VERCEL) {
   const port = Number(process.env.PORT) || 8080;
-  server.listen(port, () => console.log(`VanilliaPXY listening on ${port}`));
+  server.listen(port, () => console.log(`VanilliaPXY is listening on ${port}`));
 }
 
 export default server;
